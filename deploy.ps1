614d1672b45154aadc16ca2743b75e25a2182551
Eric Schultz <wwahammy@gmail.com>
2014-01-07 11:34:51 -0600

    Appveyor deployment for pushing the NuGet package

---
+++ deploy.ps1
@@ -0,0 +1,26 @@
+﻿Param (
+    $variables = @{},   
+    $artifacts = @{},
+    $scriptPath,
+    $buildFolder,
+    $srcFolder,
+    $outFolder,
+    $tempFolder,
+    $projectName,
+    $projectVersion,
+    $projectBuildNumber,
+    $secureNuGetApiKey,
+    $nugetApiUri
+)
+
+# list all artifacts
+foreach($artifact in $artifacts.values)
+{
+    Write-Output "Artifact: $($artifact.name)"
+    Write-Output "Type: $($artifact.type)"
+    Write-Output "Path: $($artifact.path)"
+    Write-Output "Url: $($artifact.url)"
+}
+
+cd $srcFolder
+.\.nuget\nuget.exe push $artifacts[0].path $secureNuGetApiKey -Source $nugetApiUri
